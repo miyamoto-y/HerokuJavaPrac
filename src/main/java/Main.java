92d0fca06249d0dcfb699258a21ffb301163575c
miyamoto-y <miyamoto-y@subarucom.co.jp>
2017-05-09 15:27:40 +0900

    [add] secondCommit

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,15 +1,15 @@
 import java.io.IOException;
+
 import javax.servlet.ServletException;
-import javax.servlet.http.*;
-import org.eclipse.jetty.server.Server;
-import org.eclipse.jetty.servlet.*;
-import java.net.URI;
-import java.net.URISyntaxException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 
 public class Main extends HttpServlet {
   @Override
   protected void doGet(HttpServletRequest req, HttpServletResponse resp)
       throws ServletException, IOException {
-    resp.getWriter().print("Hello from Java!");
+    resp.getWriter().println("Hello from Java!!!!");
+    resp.getWriter().println("Hello Heroku on Java!");
   }
 }
