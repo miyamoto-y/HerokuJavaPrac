76af3bced05e01e4e89d84423daea9a648ab88ca
miyamoto-y <miyamoto-y@subarucom.co.jp>
2017-05-09 15:37:14 +0900

    [MOD]3rdCommit

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -10,6 +10,6 @@ public class Main extends HttpServlet {
   protected void doGet(HttpServletRequest req, HttpServletResponse resp)
       throws ServletException, IOException {
     resp.getWriter().println("Hello from Java!!!!");
-    resp.getWriter().println("Hello Heroku on Java!");
+    // resp.getWriter().println("Hello Heroku on Java!");
   }
 }
